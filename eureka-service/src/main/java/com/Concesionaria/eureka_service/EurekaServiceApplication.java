4bdf562f1a8df029b184b62b1d9743c736993ce0
Axel Sebastian Vega <axelseba483@gmail.com>
2025-08-14 20:22:14 -0300

    integracion de gateway

--- eureka-service/src/main/java/com/Concesionaria/eureka_service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/Concesionaria/eureka_service/EurekaServiceApplication.java
@@ -2,12 +2,15 @@ package com.Concesionaria.eureka_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
+
+        SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
